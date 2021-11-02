20def74acc9ba9ff0f845d471b9fbf4ef8f84cd9
ji-vvon <jjw9895@duksung.ac.kr>
2021-11-02 20:25:10 +0900

    등록 모델 생성

--- billim/book/models.py
+++ billim/book/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+status_select = (
+    ('대여 가능', '대여 가능'),
+    ('대여중', '대여중'),
+)
+
+class Register(models.Model):
+    title = models.CharField(max_length=30)
+    author = models.CharField(max_length=30)
+    publisher = models.CharField(max_length=30)
+    pub_date = models.DateField()
+    # category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    
+    info_text = models.TextField()
+    # image = models.ImageField(upload_to='book/images/', blank=True)
+    # uploader = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=status_select, default='대여가능')
+
+    def __str__(self):
+        return self.title
