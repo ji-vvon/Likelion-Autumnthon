083270ec2631cfcc9344ce5881e7095f1c3c12d5
yeonlim <yeonlimj@gmail.com>
2021-11-04 22:42:10 +0900

    solution 기능 구현

--- solution/models.py
+++ solution/models.py
@@ -5,10 +5,9 @@ from imagekit.processors import ResizeToFill
 
 class Solution(models.Model):
     title = models.CharField(max_length=50) #제목
-    # writer = models.CharField(max_length=50, null=False) #글쓴이
-    # writer=models.ForeignKey('account.CustomUser', on_delete=models.CASCADE, null=False) #글쓴이
+    writer =  models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)#글쓴이
     content = models.TextField() #내용
-    pub_date=models.DateTimeField()
+    pub_date = models.DateTimeField()
     img = models.ImageField(upload_to="book/", blank = True, null = True)
 
     def __str__(self):
