3bbcca8e89e7782f42788fb6da478def92538962
CheolhyunPark <pch5445@gmail.com>
2021-11-05 17:38:46 +0900

    로그인 작업 중

--- account/forms.py
+++ account/forms.py
@@ -5,3 +5,9 @@ class SignupForm(UserCreationForm):
     class Meta:
         model=CustomUser
         fields=['username','password1','password2','email']
+        labels = {
+            'username' : '아이디',
+            'password1' : '패스워드',
+            'password2' : '패스워드2',
+            'email' : '이메일',
+        }
