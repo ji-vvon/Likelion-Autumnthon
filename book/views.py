ab5f7277482f826cf7e79e6f5bb81a40f3750fc6
ji-vvon <jjw9895@duksung.ac.kr>
2021-11-05 18:14:28 +0900

    카테고리 수정2

--- book/views.py
+++ book/views.py
@@ -98,39 +98,44 @@ def mypage(request):
                                             })
 
 def category_IT(request):
+    category = 'IT'
     books = MajorBook.objects.all().filter(category='IT').order_by('-id')
     paginator = Paginator(books, 8)
     page = request.GET.get('page')
     posts = paginator.get_page(page) 
-    return render(request, 'rental_main.html', {'books' : books, 'posts' : posts})
+    return render(request, 'rental_main.html', {'books' : books, 'posts' : posts, 'category': category})
 
 def category_society(request):
+    category = '사회'
     books = MajorBook.objects.all().filter(category='사회').order_by('-id')
     paginator = Paginator(books, 8)
     page = request.GET.get('page')
     posts = paginator.get_page(page) 
-    return render(request, 'rental_main.html', {'books' : books, 'posts' : posts})
+    return render(request, 'rental_main.html', {'books' : books, 'posts' : posts, 'category': category})
 
 def category_science(request):
+    category = '과학'
     books = MajorBook.objects.all().filter(category='과학').order_by('-id')
     paginator = Paginator(books, 8)
     page = request.GET.get('page')
     posts = paginator.get_page(page) 
-    return render(request, 'rental_main.html', {'books' : books, 'posts' : posts})
+    return render(request, 'rental_main.html', {'books' : books, 'posts' : posts, 'category': category})
 
 def category_art(request):
+    category = '예술'
     books = MajorBook.objects.all().filter(category='예술').order_by('-id')
     paginator = Paginator(books, 8)
     page = request.GET.get('page')
     posts = paginator.get_page(page) 
-    return render(request, 'rental_main.html', {'books' : books, 'posts' : posts})
+    return render(request, 'rental_main.html', {'books' : books, 'posts' : posts, 'category': category})
 
 def category_etc(request):
+    category = '기타'
     books = MajorBook.objects.all().filter(category='기타').order_by('-id')
     paginator = Paginator(books, 8)
     page = request.GET.get('page')
     posts = paginator.get_page(page) 
-    return render(request, 'rental_main.html', {'books' : books, 'posts' : posts})
+    return render(request, 'rental_main.html', {'books' : books, 'posts' : posts, 'category': category})
 
 
 def myborrowed_book(request):
