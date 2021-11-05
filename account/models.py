6efd91fbd8d661648736a82ab2d0be05067c2b17
bosungpark <81157873+bosungpark@users.noreply.github.com>
2021-11-05 19:18:53 +0900

    Update models.py
    회원가입 주소추가

--- account/models.py
+++ account/models.py
@@ -4,3 +4,4 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     email=models.CharField(max_length=500)
+    address=models.CharField(max_length=500, null=True)
