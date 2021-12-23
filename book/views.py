433bd738be5fa7d8d6b71d0b856b9870680cf37e
bosungpark <qkrqhtjd0806@naver.com>
2021-12-23 17:24:30 +0900

    코인 기능 및 대여하기 함수  수정

--- book/views.py
+++ book/views.py
@@ -51,14 +51,10 @@ def new(request):
             post = form.save(commit = False)
             post.upload_date = timezone.now()
             post.uploader = request.user
-            post.save()    
-        user=UserChangeForm(instance = request.user).save(commit=False)#대여시 코인을 차감해줍니다
-        if user.coin:
-            if user.coin>0:
-                user.coin+=2
-                user.save()
-            return redirect('detail', post.id)
-        return redirect('book_list')
+            post.save()           
+        else:
+            messages.error(request, '형식에 맞게 다시 등록해주세요')
+        return redirect('book_list') 
     else:  #글을 쓰기위해 들어갔을 때
         form = BookForm()
         return render(request,'rental_new.html', {'form':form})
@@ -92,20 +88,29 @@ def rental(request, id):
     rental_status=rental_book.status #대여여부
     
     if rental_status == '대여 가능':
-        user=UserChangeForm(instance = request.user).save(commit=False)#대여시 코인을 차감해줍니다
+        user=UserChangeForm(instance = request.user).save(commit=False)#대여시 빌린이의 코인을 차감해줍니다
         if user.coin:
-            if user.coin>0:
-                user.coin-=1
-                user.save()
+            if user.coin>0:#코인이 1개 이상일 때만, 대출이 가능합니다.
+                if rental_book.uploader != user: #자신이 올린 책을 스스로 빌릴 수는 없습니다.
+                    user.coin-=1
+                    user.save()
 
-                rental_book.status = '대여중'
-                rental_book.save()
-                messages.success(request, '대여가 성공했습니다!')
+                    rental_book.status = '대여중'
+                    rental_book.save()
 
-                form=BorrowedBookForm().save(commit=False) #여기부터는 내가 빌린 책 기능을 위해 데이터를 넘겨주는 부분입니다.
-                form.borrower=request.user #로그인 된 유저를 빌린이에 추가합니다
-                form.borrow_book=rental_book#책을 외래키로 저장합니다
-                BorrowedBook=form.save()          
+                    #등록한 책이 대여 되었을 때, 업로더의 코인 +2 해줍니다.
+                    uploader=UserChangeForm(instance = rental_book.uploader).save(commit=False)
+                    uploader.coin+=2
+                    uploader.save()
+
+                    messages.success(request, '대여가 성공했습니다!')
+
+                    form=BorrowedBookForm().save(commit=False) #여기부터는 내가 빌린 책 기능을 위해 데이터를 넘겨주는 부분입니다.
+                    form.borrower=request.user #로그인 된 유저를 빌린이에 추가합니다
+                    form.borrow_book=rental_book#책을 외래키로 저장합니다
+                    BorrowedBook=form.save()   
+                else:
+                      messages.success(request, '자신이 등록한 책은 대여하실 수 없습니다!')     
         else:
             messages.success(request, '코인이 부족합니다!')
 
